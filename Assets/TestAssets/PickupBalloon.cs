ada0040b324b2647e4450ec1e44bafcb7fd66512
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2020-02-04 11:37:33 +0100

    BalloonTestPowerup

---
+++ Assets/TestAssets/PickupBalloon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBalloon : MonoBehaviour
+{
+    public GameObject player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.AddComponent<BalloonPower>();
+            Destroy(this.gameObject);
+        }
+    }
+}

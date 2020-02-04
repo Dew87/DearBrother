e9f85bf8a0ec51f315714e18dcf7fce3203992e1
Benjamin Larsson <benjamin@nallebeorn.com>
2020-02-04 15:10:39 +0100

    Player movement fixes. Raycasts instead of body casts. In-progress kinematic controller.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,32 +26,35 @@ public class PlayerMovement : MonoBehaviour
 
 	public bool balloonPower = false;
 
-	private const float groundRayDistance = 0.05f;
+	private const float raycastDistance = 0.05f;
+	private const float collisionTestOffset = 0.02f;
 
-	private Vector2 velocity;
+	[SerializeField] private Vector2 velocity;
 	private float jumpGraceTimer = 0;
 	private float jumpBufferTimer = 0;
 
 	private Rigidbody2D rb2d;
 	private new BoxCollider2D collider;
+	private int solidMask;
 
 	void Awake()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 		collider = GetComponent<BoxCollider2D>();
+		solidMask = LayerMask.GetMask("Solid");
 	}
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
 		RaycastHit2D[] results = new RaycastHit2D[1];
 
-		bool grounded = rb2d.Cast(Vector2.down, results, groundRayDistance) > 0;
+		bool grounded = CheckRaycasts(Vector2.down);
 		if (grounded)
 		{
 			jumpGraceTimer = jumpGracePeriod;
 		}
-		bool headCollision = rb2d.Cast(Vector2.up, results, groundRayDistance) > 0;
+		bool headCollision = CheckRaycasts(Vector2.up);
 
 		float move = Input.GetAxisRaw("Horizontal");
 
@@ -107,10 +110,76 @@ public class PlayerMovement : MonoBehaviour
 			}
 
 			velocity.y = Mathf.Max(velocity.y, balloonPower ? -balloonFallMaxSpeed : -fallMaxSpeed);
-
 		}
 
+		//MoveAndSlide(velocity * Time.deltaTime);
 		rb2d.velocity = velocity;
 
+	}
+
+	private bool CheckRaycasts(Vector2 direction)
+	{
+		Bounds bounds = collider.bounds;
+
+		if (direction.x == 0)
+		{
+			float y = direction.y < 0 ? bounds.min.y : bounds.max.y;
+			RaycastHit2D hitL = Physics2D.Raycast(new Vector2(bounds.min.x, y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitM = Physics2D.Raycast(new Vector2(bounds.center.x, y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitR = Physics2D.Raycast(new Vector2(bounds.max.x, y), direction, raycastDistance, solidMask);
+			return hitL.collider || hitM.collider || hitR.collider;
+		}
+		else if (direction.y == 0)
+		{
+			float x = direction.x < 0 ? bounds.min.x : bounds.max.x;
+			RaycastHit2D hitU = Physics2D.Raycast(new Vector2(x, bounds.min.y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitM = Physics2D.Raycast(new Vector2(x, bounds.center.y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitD = Physics2D.Raycast(new Vector2(x, bounds.max.y), direction, raycastDistance, solidMask);
+			return hitU.collider || hitM.collider || hitD.collider;
+		}
+
+		Debug.LogError("Invalid direction " + direction, this);
+		return false;
+	}
+
+	bool DetectGround()
+	{
+		Bounds bbox = collider.bounds;
+		RaycastHit2D hitCenter = Physics2D.Raycast(new Vector2(bbox.center.x, bbox.min.y - 0.01f), Vector2.down, raycastDistance);
+		Debug.DrawRay(new Vector2(bbox.center.x, bbox.min.y), Vector2.down * raycastDistance, Color.yellow);
+		return hitCenter.collider != null;
+	}
+
+	private Vector2 MoveAndCollide(Vector2 movement)
+	{
+		Vector2 direction = movement.normalized;
+		float length = movement.magnitude;
+		if (length > 0)
+		{
+			RaycastHit2D[] hits = new RaycastHit2D[1];
+			int numHits = rb2d.Cast(direction, hits, length);
+			if (numHits > 0)
+			{
+				RaycastHit2D hit = hits[0];
+				Vector2 move = direction * (hit.distance - Physics2D.defaultContactOffset);
+				rb2d.position += move;
+				return move;
+			}
+			else
+			{
+				rb2d.position += movement;
+				return movement;
+			}
+		}
+
+		return Vector2.zero;
+	}
+
+	private Vector2 MoveAndSlide(Vector2 movement)
+	{
+		Vector2 moved = MoveAndCollide(movement);
+		Vector2 remainingMovement = movement - moved;
+
+		return moved;
 	}
 }

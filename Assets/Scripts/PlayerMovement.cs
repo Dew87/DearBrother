8a62292713f4c5ec985bf04a825a2a32e8d80289
Benjamin Larsson <benjamin@nallebeorn.com>
2020-02-04 11:47:39 +0100

    Terrible jumping without collision.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,38 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Ground movement")]
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
     public float acceleration = 20f;
     public float deceleration = 20f;
 
-    private Rigidbody2D rb2d;
+    [Header("Air movement")]
+    public float airAcceleration = 20f;
+    public float airDeceleration = 0f;
+
+    [Header("Jumping")]
+    public float jumpSpeed = 20f;
+    public float ascendGravity = 50f;
+    public float descendGravity = 100f;
+    public float jumpStopSpeed = 2f;
+
+    private const float groundRayDistance = 0.02f;
+
     private Vector2 velocity;
+
+    private Rigidbody2D rb2d;
+    private new BoxCollider2D collider;
 
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool grounded = rb2d.position.y <= -3.5f;
+
+        // Horizontal movement //
         float move = Input.GetAxisRaw("Horizontal");
 
         float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
 
         if (Mathf.Abs(velocity.x) > speed || move == 0)
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, (grounded ? deceleration : airDeceleration) * Time.deltaTime);
         }
         else
         {
-            float effectiveAccel = acceleration;
+            float effectiveAccel = grounded ? acceleration : airAcceleration;
             if (Mathf.Sign(move) == -Mathf.Sign(velocity.x))
             {
-                effectiveAccel += deceleration;
+                effectiveAccel += grounded ? deceleration : airDeceleration;
             }
             velocity.x = Mathf.MoveTowards(velocity.x, speed * Mathf.Sign(move), effectiveAccel * Time.deltaTime);
+        }
+
+        // Jumping //
+        //var results = new RaycastHit2D[1];
+        //bool grounded = rb2d.Cast(Vector2.down, results, groundRayDistance) > 0;
+
+        if (grounded)
+        {
+            velocity.y = 0;
+            rb2d.position = new Vector2(rb2d.position.x, -3.5f);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = jumpSpeed;
+            } 
+        }
+        else
+        {
+            float gravity = velocity.y > 0 ? ascendGravity : descendGravity;
+            velocity.y -= Time.deltaTime * gravity;
+            if (!Input.GetKey(KeyCode.Space) && velocity.y > jumpStopSpeed)
+            {
+                velocity.y = jumpStopSpeed;
+            }
         }
 
         

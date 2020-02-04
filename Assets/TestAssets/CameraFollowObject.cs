91755eb98812069c6e3bcb994623435702ac7633
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2020-02-04 11:02:57 +0100

    TestGrayBoxScene

---
+++ Assets/TestAssets/CameraFollowObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour
+{
+    public GameObject objectToFollow;
+    public Vector2 followOffset;
+    private void Update()
+    {
+        transform.position = objectToFollow.transform.position + (Vector3)followOffset;
+    }
+}

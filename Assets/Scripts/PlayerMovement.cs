f03f89bf559a5684309b43d534fe6502752d82ac
Dew87 <oslof87@gmail.com>
2020-02-06 14:42:58 +0100

    Revert "Merge pull request #2 from RasmusEkengren/David"
    This reverts commit adf7933ecbceeeca6da437d53711071eccb513ce.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,26 +21,25 @@ public class PlayerMovement : MonoBehaviour
 	public float jumpStopSpeed = 2f;
 	public float jumpGracePeriod = 0.1f;
 	public float jumpBufferPeriod = 0.1f;
-	public float fallMaxSpeed = 20f;
+    public float fallMaxSpeed = 20f;
 	public float balloonFallMaxSpeed = 4f;
-	public int maxJumpAmount = 1;
+    public int maxJumpAmount = 1;
 
-	[Header("WallJumping")]
-	public float wallJumpGracePeriod = 0.2f;
-	public float wallJumpSpeed = 5f;
-	public float wallSlideSpeed = 1f;
-
-	[Header("Collision")]
-	public const float castDistance = 0.05f;
+    [Header ("WallJumping")]
+    public float wallJumpGracePeriod = 0.2f;
+    public float wallJumpSpeed = 5f;
+    public float wallSlideSpeed = 1f;
 
 	[Header("Debug")]
 
 	public bool hasBalloonPower = false;
-	public int availableJumps = 0;
+    public int availableJumps = 0;
 
 	public float jumpGraceTimer = 0;
 	public float jumpBufferTimer = 0;
-	public float wallJumpGraceTimer = 0;
+    public float wallJumpGraceTimer = 0;
+
+	private const float raycastDistance = 0.05f;
 
 	[SerializeField] private Vector2 velocity;
 
@@ -48,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
 	private new BoxCollider2D collider;
 	private int solidMask;
 
-	private void Start()
+	void Awake()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 		collider = GetComponent<BoxCollider2D>();
@@ -68,12 +67,12 @@ public class PlayerMovement : MonoBehaviour
 		}
 	}
 
-	private void FixedUpdate()
+	void FixedUpdate()
 	{
-		bool collidesDown = CheckBoxcasts(Vector2.down);
-		bool collidesUp = CheckBoxcasts(Vector2.up);
-		bool collidesLeft = CheckBoxcasts(Vector2.left);
-		bool collidesRight = CheckBoxcasts(Vector2.right);
+		bool collidesDown = CheckRaycasts(Vector2.down);
+		bool collidesUp = CheckRaycasts(Vector2.up);
+		bool collidesLeft = CheckRaycasts(Vector2.left);
+		bool collidesRight = CheckRaycasts(Vector2.right);
 
 		float move = Input.GetAxisRaw("Horizontal");
 
@@ -96,15 +95,15 @@ public class PlayerMovement : MonoBehaviour
 		if (collidesDown)
 		{
 			jumpGraceTimer = jumpGracePeriod;
-			availableJumps = maxJumpAmount;
+            availableJumps = maxJumpAmount;
 		}
-		else if (collidesLeft || collidesRight)
-		{
-			wallJumpGraceTimer = wallJumpGracePeriod;
-			availableJumps = maxJumpAmount;
-		}
+        else if (collidesLeft || collidesRight)
+        {
+            wallJumpGraceTimer = wallJumpGracePeriod;
+            availableJumps = maxJumpAmount;
+        }
 
-		if (collidesUp && velocity.y > 0)
+        if (collidesUp && velocity.y > 0)
 		{
 			velocity.y = 0;
 		}
@@ -128,12 +127,12 @@ public class PlayerMovement : MonoBehaviour
 				velocity.y = jumpSpeed;
 				jumpBufferTimer = 0;
 				jumpGraceTimer = 0;
-				availableJumps--;
-			}
+                availableJumps--;
+            }
 		}
 		else
 		{
-			float gravity = velocity.y > 0 ? ascendGravity : descendGravity;
+			float gravity = velocity.y > 0 ? ascendGravity : descendGravity; 
 			velocity.y -= Time.deltaTime * gravity; //Velocity.y will not always be 0 when falling at descendGravity 0 since ascendGravity can make Velocity.y go past 0 to negative
 			if (!Input.GetKey(KeyCode.Space) && velocity.y > jumpStopSpeed)
 			{
@@ -143,38 +142,54 @@ public class PlayerMovement : MonoBehaviour
 			velocity.y = Mathf.Max(velocity.y, hasBalloonPower ? -balloonFallMaxSpeed : -fallMaxSpeed);
 		}
 
-		if (velocity.y < wallSlideSpeed && (collidesLeft || collidesRight))
-		{
-			velocity.y = wallSlideSpeed;
-		}
+        if (velocity.y < wallSlideSpeed && (collidesLeft || collidesRight))
+        {
+            velocity.y = wallSlideSpeed;
+        }
 
-		if (wallJumpGraceTimer > 0)
-		{
-			wallJumpGraceTimer -= Time.deltaTime;
-			if (jumpBufferTimer > 0)
-			{
-				velocity.y = jumpSpeed;
-				if (collidesLeft)
-				{
-					velocity.x = wallJumpSpeed;
-				}
-				else if (collidesRight)
-				{
-					velocity.x = -wallJumpSpeed;
-				}
-				availableJumps = (availableJumps == maxJumpAmount) ? availableJumps - 1 : availableJumps;
-			}
-		}
+        if (wallJumpGraceTimer > 0)
+        {
+            wallJumpGraceTimer -= Time.deltaTime;
+            if (jumpBufferTimer > 0)
+            {
+                velocity.y = jumpSpeed;
+                if (collidesLeft)
+                {
+                    velocity.x = wallJumpSpeed;
+                }
+                else if (collidesRight)
+                {
+                    velocity.x = -wallJumpSpeed;
+                }
+                availableJumps = (availableJumps == maxJumpAmount) ? availableJumps - 1 : availableJumps;
+            }
+        }
 
-		rb2d.velocity = velocity;
+        rb2d.velocity = velocity;
 	}
 
-	private bool CheckBoxcasts(Vector2 direction)
+	private bool CheckRaycasts(Vector2 direction)
 	{
 		Bounds bounds = collider.bounds;
 
-		RaycastHit2D hit = Physics2D.BoxCast(bounds.center, bounds.size, 0f, direction, castDistance, solidMask);
+		if (direction.x == 0)
+		{
+			float y = direction.y < 0 ? bounds.min.y : bounds.max.y;
+			RaycastHit2D hitL = Physics2D.Raycast(new Vector2(bounds.min.x, y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitM = Physics2D.Raycast(new Vector2(bounds.center.x, y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitR = Physics2D.Raycast(new Vector2(bounds.max.x, y), direction, raycastDistance, solidMask);
+			return hitL.collider || hitM.collider || hitR.collider;
+		}
+		else if (direction.y == 0)
+		{
+			float x = direction.x < 0 ? bounds.min.x : bounds.max.x;
+			RaycastHit2D hitU = Physics2D.Raycast(new Vector2(x, bounds.min.y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitM = Physics2D.Raycast(new Vector2(x, bounds.center.y), direction, raycastDistance, solidMask);
+			RaycastHit2D hitD = Physics2D.Raycast(new Vector2(x, bounds.max.y), direction, raycastDistance, solidMask);
+			return hitU.collider || hitM.collider || hitD.collider;
+		}
 
-		return hit.collider != null;
+		Debug.LogError("Invalid direction " + direction, this);
+		return false;
 	}
 }

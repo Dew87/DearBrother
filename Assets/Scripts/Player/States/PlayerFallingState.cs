ab98b45c456a12b7d3ac64b807630e7c8463603b
Benjamin Larsson <mafortion.benjamin@gmail.com>
2020-02-10 14:04:42 +0100

    Volatile platforms
    Break when landing heavily on them

--- Assets/Scripts/Player/States/PlayerFallingState.cs
+++ Assets/Scripts/Player/States/PlayerFallingState.cs
@@ -5,87 +5,98 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerFallingState : PlayerState
 {
-	public float maxFallSpeed = 20;
+    public float maxFallSpeed = 20;
     [Tooltip("If the player has fallen for at least this manys seconds when landing, landing lag occurs")]
-	public float landingLagDurationThreshold = 1f;
-	public float gravity = 100;
-	public float acceleration = 20;
-	public float deceleration = 10;
+    public float landingLagDurationThreshold = 1f;
+    public float gravity = 100;
+    public float acceleration = 20;
+    public float deceleration = 10;
 
     private float landingLagTimer;
 
-	public override void Enter()
-	{
-		base.Enter();
+    public override void Enter()
+    {
+        base.Enter();
 
         landingLagTimer = landingLagDurationThreshold;
-	}
+    }
 
-	public override void Exit()
-	{
-		base.Exit();
+    public override void Exit()
+    {
+        base.Exit();
 
-		player.ResetJumpGraceTimer();
-	}
+        player.ResetJumpGraceTimer();
+    }
 
-	public override void FixedUpdate()
-	{
-		base.FixedUpdate();
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
 
-		player.MoveHorizontally(player.walkingState.speed, acceleration, deceleration);
+        player.MoveHorizontally(player.walkingState.speed, acceleration, deceleration);
 
-		player.velocity.y = Mathf.MoveTowards(player.velocity.y, -maxFallSpeed, gravity * Time.deltaTime);
+        player.velocity.y = Mathf.MoveTowards(player.velocity.y, -maxFallSpeed, gravity * Time.deltaTime);
 
         if (landingLagTimer > 0)
         {
             landingLagTimer -= Time.deltaTime;
         }
 
-		if (player.CheckBoxcast(Vector2.down))
-		{
-			if (landingLagTimer > 0)
-			{
-				if (player.velocity.x == 0)
-				{
-					player.TransitionState(player.standingState);
-				}
-				else
-				{
-					player.TransitionState(player.walkingState);
-				}
-			}
-			else
-			{
-				player.TransitionState(player.landingLagState);
-			}
-			return;
-		}
+        Collider2D ground = player.CheckBoxcast(Vector2.down);
 
-		if (player.jumpGraceTimer > 0 && player.isJumpInputPressedBuffered)
-		{
-			player.TransitionState(player.jumpingState);
-		}
-		else if (player.hasDoubleJump && player.doesDoubleJumpRemain && player.isJumpInputPressedBuffered)
-		{
-			player.TransitionState(player.doubleJumpingState);
-		}
-		else if (player.isJumpInputHeld)
-		{
-			player.TransitionState(player.glidingState);
-		}
+        if (ground)
+        {
+            if (ground.TryGetComponent<VolatilePlatform>(out VolatilePlatform platform))
+            {
+                if (player.velocity.y < -platform.breakSpeed)
+                {
+                    platform.Break(); 
+                }
+            }
+
+
+            if (landingLagTimer > 0)
+            {
+                if (player.velocity.x == 0)
+                {
+                    player.TransitionState(player.standingState);
+                }
+                else
+                {
+                    player.TransitionState(player.walkingState);
+                }
+            }
+            else
+            {
+                player.TransitionState(player.landingLagState);
+            }
+            return;
+        }
+
+        if (player.jumpGraceTimer > 0 && player.isJumpInputPressedBuffered)
+        {
+            player.TransitionState(player.jumpingState);
+        }
+        else if (player.hasDoubleJump && player.doesDoubleJumpRemain && player.isJumpInputPressedBuffered)
+        {
+            player.TransitionState(player.doubleJumpingState);
+        }
+        else if (player.isJumpInputHeld)
+        {
+            player.TransitionState(player.glidingState);
+        }
         else if (player.isGrappleButtonHeld && player.grappleDetection.grapplePoint != null)
         {
             player.TransitionState(player.grappleState);
         }
-	}
+    }
 
-	public override void Start()
-	{
-		base.Start();
-	}
+    public override void Start()
+    {
+        base.Start();
+    }
 
-	public override void Update()
-	{
-		base.Update();
-	}
+    public override void Update()
+    {
+        base.Update();
+    }
 }

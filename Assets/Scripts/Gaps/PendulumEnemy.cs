6cd51010f474e9434f3f0fc41ce8252340839ebb
Benjamin Larsson <mafortion.benjamin@gmail.com>
2020-02-10 11:58:46 +0100

    Fix warning message from PendulumEnemy OnValidate

--- Assets/Scripts/Gaps/PendulumEnemy.cs
+++ Assets/Scripts/Gaps/PendulumEnemy.cs
@@ -31,13 +31,24 @@ public class PendulumEnemy : MonoBehaviour
             float distance = Vector2.Distance(transform.position, transform.GetChild(0).position);
             float theta = (-90 + arcLength * 0.5f) * Mathf.Deg2Rad;
             Vector2 down = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
-            UnityEditor.Handles.DrawWireArc(transform.position, Vector3.back, down, arcLength, distance); 
+            UnityEditor.Handles.DrawWireArc(transform.position, Vector3.back, down, arcLength, distance);
         }
     }
 
     private void OnValidate()
     {
-        float initialRotation = Mathf.Sin(cycleOffset * Mathf.PI * 2 / period) * 0.5f * arcLength;
-        transform.rotation = Quaternion.Euler(0, 0, initialRotation);
+        IEnumerator Coroutine()
+        {
+            // Doing this in coroutine to prevent warning message about "SendMessage can't be called in OnValidate"
+            yield return null;
+
+            float initialRotation = Mathf.Sin(cycleOffset * Mathf.PI * 2 / period) * 0.5f * arcLength;
+            transform.rotation = Quaternion.Euler(0, 0, initialRotation);
+        }
+
+        if (gameObject.activeInHierarchy)
+        {
+            StartCoroutine(Coroutine()); 
+        }
     }
 }

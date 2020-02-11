38f395bbbc6b4fb60a4803a48496ce8d283563ce
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2020-02-11 12:21:24 +0100

    Added Prefabs, Changed grapple behaviors
    Small swing (added gravity multiplier) and whip (added a unity event) change, major pull changes

--- Assets/Scripts/GrapplePointBehaviour.cs
+++ Assets/Scripts/GrapplePointBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GrapplePointBehaviour : MonoBehaviour
 {
@@ -11,11 +12,18 @@ public class GrapplePointBehaviour : MonoBehaviour
 		Pull
 	}
 	public GrappleType grappleType;
+	[Space]
+	public UnityEvent whipEvent;
+	[HideInInspector]public Rigidbody2D rb2d;
+	private void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
 	public void UseGrapple()
 	{
 		if (grappleType == GrappleType.Whip)
 		{
-			Destroy(gameObject);
+			whipEvent.Invoke();
 		}
 	}
 }

26669a1fbf009a3be0ff3def0bbd5c1a7b0a8707
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2020-02-12 14:55:09 +0100

    Basic Animal and respawnFade,

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+	private IEnumerator FadeOut()
+	{
+		float alpha = 0;
+		while (alpha < 1)
+		{
+			alpha += 1 / respawnTime * Time.unscaledDeltaTime;
+			fadeOutSprite.color = new Color(fadeOutSprite.color.r, fadeOutSprite.color.g, fadeOutSprite.color.b, alpha);
+			yield return null;
+		}
+		Time.timeScale = 1;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+	public float respawnTime = 0.5f;
+	public SpriteRenderer fadeOutSprite;
+	public void Start()
+	{
+		fadeOutSprite.color = new Color(fadeOutSprite.color.r, fadeOutSprite.color.g, fadeOutSprite.color.b, 0);
+	}
 	public void TakeDamage()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Time.timeScale = 0;
+		StartCoroutine(FadeOut());
 	}
 }
---
+++ Assets/TestAssets/AnimalWhipBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalWhipBehaviour : MonoBehaviour
+{
+	public float moveTimePeriod = 0.5f;
+	public float moveSpeed = 5f;
+
+	public Sprite idleSprite;
+	public Sprite moveSprite;
+
+	private SpriteRenderer spriteRenderer;
+	private Rigidbody2D rb2d;
+
+	private float moveTimer;
+	private Vector2 direction;
+
+	private LayerMask playerMask;
+    void Start()
+    {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		rb2d = GetComponent<Rigidbody2D>();
+		spriteRenderer.sprite = idleSprite;
+    }
+    void Update()
+    {
+        if (moveTimer > 0)
+		{
+			moveTimer -= Time.deltaTime;
+			spriteRenderer.sprite = moveSprite;
+			rb2d.velocity = direction * moveSpeed;
+		}
+		else
+		{
+			spriteRenderer.sprite = idleSprite;
+			rb2d.velocity = Vector2.zero;
+		}
+    }
+	public void Whip(GameObject player)
+	{
+		moveTimer = moveTimePeriod;
+		if (player.transform.position.x > transform.position.x)
+		{
+			if (Mathf.Approximately(transform.rotation.eulerAngles.y, 0))
+			{
+				transform.Rotate(Vector3.up, 180);
+			}
+			direction = Vector2.left;
+		}
+		else
+		{
+			if (Mathf.Approximately(transform.rotation.eulerAngles.y, 180))
+			{
+				transform.Rotate(Vector3.up, 180);
+			}
+			direction = Vector2.right;
+		}
+	}
+}

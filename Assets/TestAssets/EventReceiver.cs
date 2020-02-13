f5545b567e70d6c2bda013be73f0c7ffc9266da2
David Erikssen <oslof87@gmail.com>
2020-02-13 10:33:50 +0100

    EventManager
    EventManager added

---
+++ Assets/TestAssets/EventReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventReceiver : MonoBehaviour
+{
+	public string testEvent = "TestEvent";
+
+	private void OnDisable()
+	{
+		EventManager.StopListening(testEvent, OnTestEvent);
+	}
+
+	private void OnEnable()
+	{
+		EventManager.StartListening(testEvent, OnTestEvent);
+	}
+
+	private void OnTestEvent()
+	{
+		Debug.Log(testEvent + " received by " + this.ToString());
+	}
+}

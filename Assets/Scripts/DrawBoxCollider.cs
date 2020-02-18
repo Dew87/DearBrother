6e793aacb1e582734050fce5e5cade84d8e1739d
Benjamin Larsson <mafortion.benjamin@gmail.com>
2020-02-18 11:32:53 +0100

    Killbox prefab

---
+++ Assets/Scripts/DrawBoxCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBoxCollider : MonoBehaviour
+{
+	public Color color = new Color(1, 0, 0.48f, 0.69f);
+
+	private void OnDrawGizmos()
+	{
+		if (TryGetComponent<BoxCollider2D>(out BoxCollider2D collider))
+		{
+			Gizmos.color = color;
+			Bounds bounds = collider.bounds;
+			Gizmos.DrawCube(bounds.center, bounds.size);
+		}
+	}
+}

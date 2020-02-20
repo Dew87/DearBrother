390b9615fbcc9bb733269fcef3b4d17851134145
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2020-02-20 14:51:02 +0100

    Added Deceleration and waiting for MovingPlatform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -8,7 +8,9 @@ public class MovingPlatform : MonoBehaviour
 	public List<Transform> pathPoints;
 	public bool isMovingOneDirection = false;
 	public float moveSpeed = 5f;
-	public float tolerance = 1f;
+	public float waitTime = 1f;
+	public float tolerance = 0.1f;
+	public float decelerationDistance = 1f;
 
 	private List<Collider2D> childrenColliders = new List<Collider2D>();
 
@@ -16,6 +18,8 @@ public class MovingPlatform : MonoBehaviour
 
 	private int currentPositionInPath;
 	private bool isMovingForward;
+	private float waitTimer = 0f;
+	private float currentMoveSpeed;
 
 	private void OnDrawGizmos()
 	{
@@ -58,31 +62,9 @@ public class MovingPlatform : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		transform.position = Vector2.MoveTowards(transform.position, pathPoints[currentPositionInPath].position, Time.deltaTime * moveSpeed);
-		if (Vector2.Distance(transform.position, pathPoints[currentPositionInPath].position) < tolerance)
+		if (waitTimer <= 0)
 		{
-			if (!isMovingOneDirection)
-			{
-				currentPositionInPath++;
-				if (currentPositionInPath == pathPoints.Count)
-				{
-					currentPositionInPath = 0;
-				}
-			}
-			else
-			{
-				currentPositionInPath += isMovingForward ? 1 : -1;
-				if (currentPositionInPath == pathPoints.Count)
-				{
-					isMovingForward = false;
-					currentPositionInPath = pathPoints.Count - 1;
-				}
-				else if (currentPositionInPath == -1)
-				{
-					isMovingForward = true;
-					currentPositionInPath = 1;
-				}
-			}
+			transform.position = Vector2.MoveTowards(transform.position, pathPoints[currentPositionInPath].position, Time.deltaTime * currentMoveSpeed);
 		}
     }
 	private void Update()
@@ -110,6 +92,49 @@ public class MovingPlatform : MonoBehaviour
 			{
 				childrenColliders[i].gameObject.transform.parent.parent = null;
 				childrenColliders.Remove(childrenColliders[i]);
+			}
+		}
+		float currentDistanceToNextPoint = Vector2.Distance(transform.position, pathPoints[currentPositionInPath].position);
+		currentMoveSpeed = moveSpeed;
+		if (isMovingOneDirection)
+		{
+			if (waitTimer > 0)
+			{
+				waitTimer -= Time.deltaTime;
+			}
+			if (currentPositionInPath == pathPoints.Count - 1 || currentPositionInPath == 0)
+			{
+				if (currentDistanceToNextPoint < decelerationDistance)
+				{
+					currentMoveSpeed = (currentDistanceToNextPoint / decelerationDistance) * moveSpeed;
+				}
+			}
+		}
+		if (currentDistanceToNextPoint < tolerance)
+		{
+			if (!isMovingOneDirection)
+			{
+				currentPositionInPath++;
+				if (currentPositionInPath == pathPoints.Count)
+				{
+					currentPositionInPath = 0;
+				}
+			}
+			else
+			{
+				currentPositionInPath += isMovingForward ? 1 : -1;
+				if (currentPositionInPath == pathPoints.Count)
+				{
+					waitTimer = waitTime;
+					isMovingForward = false;
+					currentPositionInPath = pathPoints.Count - 1;
+				}
+				else if (currentPositionInPath == -1)
+				{
+					waitTimer = waitTime;
+					isMovingForward = true;
+					currentPositionInPath = 1;
+				}
 			}
 		}
 	}

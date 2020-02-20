cf39737ecffa52f42e9ad4038fb2328ffb13b327
Benjamin Larsson <mafortion.benjamin@gmail.com>
2020-02-20 13:30:56 +0100

    Fix error in FrightenAnimal when collider has no parent

--- Assets/Scripts/Gaps/FrightenAnimal.cs
+++ Assets/Scripts/Gaps/FrightenAnimal.cs
@@ -6,7 +6,8 @@ public class FrightenAnimal : MonoBehaviour
 {
 	private void OnTriggerStay2D(Collider2D collision)
 	{
-		if (collision.transform.parent.TryGetComponent<AnimalWhipBehaviour>(out AnimalWhipBehaviour animal))
+		Transform parent = collision.transform.parent;
+		if (parent && parent.TryGetComponent<AnimalWhipBehaviour>(out AnimalWhipBehaviour animal))
 		{
 			Debug.Log("Frighten animal");
 			animal.Frighten(new Vector2(Mathf.Sign(animal.transform.position.x - transform.position.x), 0));

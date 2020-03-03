cd3962b536a951a6961f49e2df0ffe7bd16d2a2f
Benjamin <mafortion.benjamin@gmail.com>
2020-03-03 19:33:43 +0100

    Show uncollected memories in pause menu

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -7,13 +7,39 @@ public class Collectible : MonoBehaviour
 {
 	public string animationName;
 	[Space()]
-	public bool collected = false;
+	public bool isCollected = false;
+
+	public static List<Collectible> list { get; private set; }
+	private static bool isListDirty;
 
 	Flowchart flowchart;
 
 	private void Awake()
 	{
 		flowchart = GetComponent<Flowchart>();
+		if (list == null)
+		{
+			list = new List<Collectible>(4);
+		}
+		list.Add(this);
+		isListDirty = true;
+	}
+
+	private void Start()
+	{
+		if (isListDirty)
+		{
+			isListDirty = false;
+			Debug.Log("Sort collectibles list");
+			// Sort collectibles by order in hierarchy (or rather sibling index -- so it assumes all collectibles have the same parent)
+			list.Sort((a, b) => a.transform.GetSiblingIndex().CompareTo(b.transform.GetSiblingIndex()));
+		}
+	}
+
+	private void OnDestroy()
+	{
+		list.Remove(this);
+		isListDirty = true;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -34,7 +60,7 @@ public class Collectible : MonoBehaviour
 	private IEnumerator DoShowMemory()
 	{
 		yield return StartCoroutine(MemoryController.get.Open(animationName));
-		
+
 		flowchart.ExecuteBlock("Collected");
 		while (flowchart.HasExecutingBlocks())
 		{
@@ -46,6 +72,6 @@ public class Collectible : MonoBehaviour
 		Time.timeScale = 1;
 		GetComponent<SpriteRenderer>().enabled = false;
 		GetComponent<Collider2D>().enabled = false;
-		collected = true;
+		isCollected = true;
 	}
 }

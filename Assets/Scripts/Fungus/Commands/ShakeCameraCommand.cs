7b080df3e4085bd58b7e4a78aad1f89ca99d405f
Benjamin Larsson <mafortion.benjamin@gmail.com>
2020-03-04 13:25:26 +0100

    Camera shake
    Not used in many places yet, but it exists.

---
+++ Assets/Scripts/Fungus/Commands/ShakeCameraCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("*DearBrother", "Shake Camera", "")]
+[AddComponentMenu("")]
+public class ShakeCameraCommand : Command
+{
+	[Range(0, 1)] public float intensity;
+	[Range(0, 1)] public float rotationalIntensity;
+
+	public override void OnEnter()
+	{
+		base.OnEnter();
+
+		CameraShake.get.Shake(intensity, rotationalIntensity);
+		Continue();
+	}
+
+	public override string GetSummary()
+	{
+		return "Intensity: " + intensity + ", Rotational: " + rotationalIntensity;
+	}
+
+	public override Color GetButtonColor()
+	{
+		return new Color32(216, 228, 170, 255);
+	}
+}

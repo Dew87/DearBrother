3831d25d5ae26a2ce218acf70b7103727f6af63f
David Erikssen <Oslof87@gmail.com>
2020-03-04 16:11:04 +0100

    TarMan changes and inputThreshold removed

---
+++ Assets/Scripts/Gaps/TarMan/States/TarManIdleState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TarManIdleState : TarManState
+{
+	public override void Enter()
+	{
+		base.Enter();
+
+		tarMan.rb2d.velocity = Vector2.zero;
+	}
+}

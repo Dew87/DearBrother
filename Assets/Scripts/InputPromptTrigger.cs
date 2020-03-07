58c36ed879531f11ea198af6e0aa896cdd8ef36f
Benjamin <mafortion.benjamin@gmail.com>
2020-03-07 23:49:14 +0100

    Input prompts

---
+++ Assets/Scripts/InputPromptTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPromptTrigger : MonoBehaviour
+{
+	public bool hide = false;
+	public Sprite sprite;
+	public string text;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			if (hide)
+			{
+				InputPrompt.Hide();
+			}
+			else
+			{
+				InputPrompt.Show(text, sprite); 
+			}
+			gameObject.SetActive(false);
+		}
+	}
+}

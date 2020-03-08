ff33e9ce87255f9f3102b3508d1f289ac961dae0
Benjamin <mafortion.benjamin@gmail.com>
2020-03-09 00:12:48 +0100

    Disallow mouse input in pause menu
    Do we even want that? I don't know, let's find out!

---
+++ Assets/Scripts/UI/PreventLosingFocus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/* Either me or Unity is dumb, I should not have to do this
+ * It seems like even with the Graphic Raycaster disabled,
+ * clicking outside any buttons will still make them lose focus.
+ * This is a workaround for that.
+ */
+
+public class PreventLosingFocus : MonoBehaviour
+{
+	GameObject lastSelected;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		lastSelected = EventSystem.current.currentSelectedGameObject;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (EventSystem.current.currentSelectedGameObject == null)
+		{
+			EventSystem.current.SetSelectedGameObject(lastSelected);
+		}
+
+		lastSelected = EventSystem.current.currentSelectedGameObject;
+	}
+}

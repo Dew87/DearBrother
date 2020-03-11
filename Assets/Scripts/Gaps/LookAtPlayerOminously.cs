f9eb46006b6ff0842ad6e45905073c5e0a69403e
Benjamin <mafortion.benjamin@gmail.com>
2020-03-11 22:10:23 +0100

    Make dripper look at player

---
+++ Assets/Scripts/Gaps/LookAtPlayerOminously.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayerOminously : MonoBehaviour
+{
+	public Sprite[] sprites;
+	public float maxAngle = 30f;
+	public float rotateSpeed = 30f;
+
+	public float currentAngle;
+	private SpriteRenderer spriteRenderer;
+
+	private void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 meToPlayer = PlayerController.get.transform.position - transform.position;
+		float angleToPlayer = Mathf.Atan2(meToPlayer.y, meToPlayer.x) * Mathf.Rad2Deg + 90f;
+		if (angleToPlayer > maxAngle)
+		{
+			currentAngle = Mathf.MoveTowards(currentAngle, 0, rotateSpeed * Time.deltaTime);
+		}
+		else
+		{
+			currentAngle = Mathf.MoveTowards(currentAngle, angleToPlayer, rotateSpeed * Time.deltaTime);
+		}
+
+		float normalizedAngle = Mathf.InverseLerp(-maxAngle, maxAngle, currentAngle);
+		normalizedAngle = Mathf.Clamp01(normalizedAngle);
+
+		int numSprites = sprites.Length;
+		int spriteIndex = Mathf.RoundToInt(normalizedAngle * (numSprites - 1));
+		spriteRenderer.sprite = sprites[spriteIndex];
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawLine(transform.position, transform.position + Util.Polar(5f, -90f - maxAngle));
+		Gizmos.DrawLine(transform.position, transform.position + Util.Polar(5f, -90f + maxAngle));
+	}
+}
--- Assets/Scripts/Util/Util.cs
+++ Assets/Scripts/Util/Util.cs
@@ -8,4 +8,9 @@ public static class Util
 	{
 		return new Vector3(Mathf.SmoothStep(from.x, to.x, t), Mathf.SmoothStep(from.y, to.y, t), Mathf.SmoothStep(from.z, to.z, t));
 	}
+
+	public static Vector3 Polar(float length, float angle)
+	{
+		return new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0) * length;
+	}
 }
